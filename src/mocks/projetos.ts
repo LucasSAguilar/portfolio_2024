52ce3ee034552f7d2b364eeccac5725edc866ec0
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-03-18 12:55:01 -0300

    Organizacao dos posts

--- src/mocks/projetos.ts
+++ src/mocks/projetos.ts
@@ -1,4 +1,6 @@
 import capa_FT_Digital from "../assets/projetos/FT_digital/capa.webp"
+import capa_Cartao_Azul from "../assets/projetos/Cartao_Azul/capa.webp"
+import capa_Commandas from "../assets/projetos/Commandas/capa.webp"
 
 const projetos = [
     {
@@ -6,28 +8,28 @@ const projetos = [
         titulo: "F.T. Digital",
         descricao: "Sistema desktop para gerir candidatos e colaboradores da empresa, contando com sistema de cadastro de currículo, evoluções sobre o indivíduo e sistema de score para definir melhores candidatos",
         video: "",
-        codigo: "a",
-        site: "a",
+        codigo: "",
+        site: "",
         capa: capa_FT_Digital
     },
     {
         id: 2,
         titulo: "Cartão Azul",
-        descricao: "Teste Cartao Azul",
+        descricao: "O 'Cartão Azul' vai além de ser um simples aplicativo de desktop; é uma ferramenta de impacto social desenvolvida com dedicação para a Assistência Social da cidade de Santa Rita do Passa Quatro. Neste projeto, criei um sistema que visa agilizar e controlar a emissão de documentos essenciais para pessoas com Transtorno do Espectro Autista (TEA), proporcionando um acesso simplificado a serviços cruciais.",
         video: "",
         codigo: "",
-        site: "b",
-        capa: "../assets/projetos/FT_digital/capa.webp"
+        site: "",
+        capa: capa_Cartao_Azul
 
     },
     {
         id: 3,
         titulo: "Commandas",
-        descricao: "Teste Commandas",
-        video: "a",
+        descricao: "O 'Commandas' é uma solução inovadora atualmente em desenvolvimento para aprimorar a gestão de pedidos em estabelecimentos que oferecem comidas e bebidas. Este sistema completo é composto por duas partes cruciais: o Desktop, uma ferramenta robusta para administração centralizada, e o Mobile, um aplicativo ágil e intuitivo para facilitar o atendimento e os pedidos dos clientes. O Commandas promete oferecer uma experiência integrada e eficiente para a indústria gastronômica.",
+        video: "https://www.youtube.com/watch?v=fDdEslJN6rk",
         codigo: "",
-        site: "a",
-        capa: "../assets/projetos/FT_digital/capa.webp"
+        site: "",
+        capa: capa_Commandas
 
     }
     
