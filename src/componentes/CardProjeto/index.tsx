902ddc4178686cffe52e8d52c3cc2ed64aa21ff1
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-03-19 13:36:01 -0300

    Inclusão dos icones dos projetos no mobile

--- src/componentes/CardProjeto/index.tsx
+++ src/componentes/CardProjeto/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import github_icon from "../../assets/icones/icon_github.svg";
 import youtube_icon from "../../assets/icones/icon_youtube.svg";
 import website_icon from "../../assets/icones/icon_website.svg";
-import capa from "../../assets/projetos/FT_digital/capa.webp"
+import capa from "../../assets/projetos/FT_digital/capa.webp";
 
 interface projetoInterface {
   id: number;
@@ -21,39 +21,39 @@ interface projetoProps {
 const CardProjeto = ({ projeto }: projetoProps) => {
   return (
     <div className="card-projeto">
-      <img alt="Banner 01" className="banner-projeto" src={projeto.capa}/>
+      <img alt="Banner 01" className="banner-projeto" src={projeto.capa} />
       <div className="escritos-card">
         <h3 className="titulo-projeto">{projeto.titulo}</h3>
         <p className="descricao-projeto">{projeto.descricao}</p>
-      </div>
-      <div className="box-icons-projeto">
-        <a href={projeto.codigo}>
-          <img
-            src={github_icon}
-            alt="Icone do github"
-            className={`icon-github ${
-              projeto.codigo !== "" ? "" : "icon-disabled"
-            }`}
-          />
-        </a>
-        <a href={projeto.site}>
-          <img
-            src={website_icon}
-            alt="Icone da web"
-            className={`icon-site ${
-              projeto.site !== "" ? "" : "icon-disabled"
-            }`}
-          />
-        </a>
-        <a href={projeto.video}>
-          <img
-            src={youtube_icon}
-            alt="Icone do youtube"
-            className={`icon-video ${
-              projeto.video !== "" ? "" : "icon-disabled"
-            }`}
-          />
-        </a>
+        <div className="box-icons-projeto">
+          <a href={projeto.codigo}>
+            <img
+              src={github_icon}
+              alt="Icone do github"
+              className={`icon-github ${
+                projeto.codigo !== "" ? "" : "icon-disabled"
+              }`}
+            />
+          </a>
+          <a href={projeto.site}>
+            <img
+              src={website_icon}
+              alt="Icone da web"
+              className={`icon-site ${
+                projeto.site !== "" ? "" : "icon-disabled"
+              }`}
+            />
+          </a>
+          <a href={projeto.video}>
+            <img
+              src={youtube_icon}
+              alt="Icone do youtube"
+              className={`icon-video ${
+                projeto.video !== "" ? "" : "icon-disabled"
+              }`}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
