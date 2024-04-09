fcbe2736696c5d1ad45a3c6e76c908f2d7ebe727
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-04-09 12:05:50 -0300

    Finalizado os markdowns

--- src/mocks/projetos.ts
+++ src/mocks/projetos.ts
@@ -112,7 +112,8 @@ const projetos = [
     tipo: "Sites expositivos",
     destaque: false,
     capa: capa_Serenatto,
-    markdown: "",
+    markdown:
+      "# Serenatto\n\nEsse é um site expositivo de uma empresa fictícia chamada 'Serenatto' com foco em venda de alimentos em gerais, principalmente relacionado a café. Todo o site foi desenvolvido durante um curso da Alura relacionado ao Bootstrap, focando em compreender seus componentes mais atuais. A simplicidade é a chave do design do Serenatto. O layout limpo e minimalista permite que os elementos essenciais se destaquem, transmitindo a atmosfera aconchegante do café. A escolha cuidadosa de cores, fontes e espaçamento contribui para uma estética equilibrada, garantindo que o visitante seja imerso na experiência sem distrações desnecessárias.\n\n## Efeito de troca de fundo\n\nUma das características mais distintivas do Serenatto é a utilização de efeitos visuais envolventes. A imagem de fundo que permanece estática enquanto o visitante rola pela página, mas que muda dinamicamente quando um elemento específico entra em foco, cria uma atmosfera única e moderna. Essa técnica não apenas captura a atenção, mas também conta uma história visual à medida que o usuário explora a página.\n\n## Tecnologias\n\n- HTML5\n- CSS3\n- Bootstrap",
   },
   {
     id: "8",
@@ -125,7 +126,8 @@ const projetos = [
     tipo: "Sites expositivos",
     destaque: false,
     capa: capa_Meteora,
-    markdown: "",
+    markdown:
+      "# Meteora\n\nEsse é um site expositivo sobre uma empresa fictícia chamada 'Meteora', da qual seria responsável pela venda de vestimentas em geral. O principal intuito era criar um site com design minimalista mas que causasse impacto para os visitantes.\n\n## Bootstrap\n\nEsse sistema foi todo construido através de um curso da *Alura* sobre *Bootstrap*, da qual teve o intuito de apresentar a ferramenta e mostrar seus componentes, facilitando a estilização e agilizando o desenvolvimento do site. Outro foco importante do projeto é o sistema de *slide* para apresentação de promoções, do qual costuma custar um tempo significativo do desenvolvedor caso queira construir do zero, mostrando a agilidade que o *bootstrap* propõe.\n\n## Tecnologias usadas\n\n- HTML5\n- CSS3\n- Bootstrap",
   },
   {
     id: "9",
@@ -138,7 +140,8 @@ const projetos = [
     tipo: "Sites expositivos",
     destaque: false,
     capa: capa_Cinetag,
-    markdown: "",
+    markdown:
+      "# Cinetag\n\nO *Cinetag* simula um sistema de *streaming* de vídeos, do qual foi cadastrado no sistemas alguns vídeos publicados no canal oficial da *Alura* do *Youtube*. Além disso, o site conta com um sistema de *favoritos*, onde o usuário pode apontar alguns vídeos que ficam anotados como prioritários para o usuário.\n\n## Técnicas usadas\n\nPara desenvolver o sistema de favoritos usamos o *useContext*, responsável por criar contextos gerais para a aplicação, mostrando-se muito valioso para desenvolvedores que usam ReactJS\n\n## Tecnologias usadas\n\n- React\n- HTML5\n- CSS3\n- Javascript",
   },
   // Jogos ----
   {
@@ -152,7 +155,7 @@ const projetos = [
     tipo: "Jogos",
     destaque: false,
     capa: capa_PK_Run,
-    markdown: "",
+    markdown: "# Pumpkin run\n\nO Pumpkin Run é um dos meus primeiros projetos em JavaScript que me introduziu ao desenvolvimento web. Neste jogo inspirado no famoso jogo do dinossauro do Google, os jogadores precisam guiar o personagem principal para desviar dos caçadores que aparecem na tela. Prepare-se para uma jornada endless runner cheia de sustos e desafios!\n\n## Conceito e Estilo Endless Runner\n\nO Pumpkin Run segue o estilo clássico de endless runner, onde o objetivo é sobreviver o máximo possível, acumulando pontos à medida que o personagem avança pela tela. A inspiração veio do icônico jogo do dinossauro do Google, mas com uma abordagem temática mais assustadora e envolvente. O desafio é manter o personagem longe dos caçadores, evitando colisões que encerram o jogo.\n\n## Desenvolvimento Inicial - Aprendizado com HTML, CSS e JS\n\nO Pumpkin Run surgiu como um dos meus primeiros projetos ao iniciar meus estudos em desenvolvimento web. Ele foi desenvolvido com HTML, CSS e JavaScript, o que me permitiu aprender os fundamentos dessas linguagens e começar a aplicá-los para criar uma experiência interativa na web.\n\n## A Gameplay e a Magia das Ações do Jogador\n\nO controle do personagem é simples e eficiente, permitindo que os jogadores pulem ou deslizem para evitar os caçadores. Ao clicar, pressionar a tecla de espaço ou deslizar o dedo para baixo (em dispositivos móveis), o personagem realiza um pulo ou slide, adicionando uma camada de estratégia e diversão à gameplay.\n\n## Desafios Crescentes e Aumento de Velocidade\n\nÀ medida que a pontuação do jogador aumenta, a velocidade da tela também aumenta progressivamente. Esse desafio progressivo mantém os jogadores envolvidos e testa suas habilidades de reação. A velocidade crescente cria uma tensão constante, incentivando os jogadores a superarem seus próprios recordes e alcançarem novas distâncias.\n\n## Atravessando a Tela e Gerando Inimigos\n\nPara aprimorar a experiência do Pumpkin Run, os inimigos (caçadores) são gerados aleatoriamente, mantendo a jogabilidade dinâmica e emocionante. Os jogadores precisam estar alertas e reagir rapidamente para desviar dos caçadores que aparecem no caminho. A criação dinâmica de inimigos torna cada partida única e imprevisível.\n\n## Resultado Final e Desafios Futuros\n\nO Pumpkin Run se tornou uma experiência de aprendizado incrível para mim, mostrando a força do HTML, CSS e JavaScript para criar jogos interativos na web. Embora seja um dos meus primeiros projetos, o Pumpkin Run ainda é um marco significativo na minha jornada como desenvolvedor.\n\n## Tecnologias usadas\n\n- Javascript\n- CSS3\n- HTML5\n- JQuery",
   },
   {
     id: "11",
@@ -165,7 +168,8 @@ const projetos = [
     tipo: "Jogos",
     destaque: false,
     capa: capa_Jogo_da_velha,
-    markdown: "",
+    markdown:
+      "# Jogo da velha\n\nO famoso *jogo da velha* criado com códigos bem simples no ínicio dos meus estudos em desenvolvimento de *software* com *javascript*\n\n## Regras\n\n- **Objetivo:** O objetivo do jogo é ser o primeiro jogador a formar uma linha de três símbolos iguais (X ou O) horizontal, vertical ou diagonalmente, dentro do tabuleiro 3x3.\n- **Tabuleiro:** O jogo é jogado em um tabuleiro quadrado 3x3. Cada célula do tabuleiro pode conter um dos dois símbolos: 'X' ou 'O', representando os dois jogadores.\n-  **Jogadas:** Os jogadores alternam entre si, colocando um símbolo de cada vez em uma célula vazia do tabuleiro.\n-  **Vitória:** Um jogador vence quando consegue formar uma linha de três símbolos iguais, seja na horizontal, vertical ou diagonal.\n-  **Empate:** Se todas as células do tabuleiro forem preenchidas e nenhum jogador conseguir formar uma linha de três símbolos iguais, o jogo termina em empate.\n- **Reinício:** Após uma partida ser concluída (com vitória ou empate), os jogadores podem optar por reiniciar o jogo, limpando o tabuleiro para uma nova partida.\n\n## Tecnologias usadas\n\n- Javascript\n- HTML5\n- CSS3",
   },
   // Ferramentas ---
   {
@@ -179,7 +183,7 @@ const projetos = [
     tipo: "Ferramentas",
     destaque: false,
     capa: capa_Organa,
-    markdown: "",
+    markdown: "# Organa\n\nO **'Organa'** foi concebido com o objetivo de criar uma ferramenta eficiente para a organização de equipes. A utilização do React foi fundamental para alcançar esse objetivo. As vantagens do React, como a criação de componentes reativos e a manipulação eficiente do VirtualDOM, proporcionaram uma interface de usuário responsiva e de alto desempenho. O fluxo unidirecional de dados (one-way data binding) e a capacidade de refatorar e evoluir os componentes de acordo com as necessidades do projeto foram cruciais para a criação de um organizador de equipes versátil e escalável.\n\n## Organização de equipes\n\nO  **'Organa'**  se destaca como um organizador de equipes completo e intuitivo. Os usuários podem criar times e cadastrar membros em cada equipe. A funcionalidade de personalização de cores para cada time proporciona uma experiência visual atraente e permite que os usuários personalizem seus espaços de trabalho.\n\nA implementação de componentes reativos no  **'Organa'**  permite que as alterações sejam refletidas em tempo real, garantindo uma experiência suave e responsiva. Através do VirtualDOM, o React gerencia eficientemente as mudanças na interface, tornando o  **'Organa'**  uma aplicação rápida e eficiente.\n\n## Tecnologias\n\n- HTML5\n- CSS3\n- Javascript\n- ReactJS",
   },
   {
     id: "13",
@@ -192,7 +196,7 @@ const projetos = [
     tipo: "Ferramentas",
     destaque: false,
     capa: capa_Aguilar_Studies,
-    markdown: "",
+    markdown: "# Aguilar Studies\n\nSistema criado para gerir o tempo de estudo através de um cronômetro, no qual o usuário da um nome ao que pretende estudar, define o tempo e escolhe quando quer inicializar.\n\n## CSS Modules para Estilos Isolados\n\nEvitar conflitos e sobreposições de estilos é fundamental para manter uma experiência de usuário coesa. No  **Aguilar-studies**, empregamos  **CSS Modules**  para garantir que os estilos aplicados em um componente não afetem outros componentes. Com essa abordagem, cada componente possui seu próprio escopo isolado de estilos, prevenindo conflitos indesejados.\n\n## Código Limpo e Documentado com Boas Práticas\n\nDesenvolver código limpo e bem documentado é essencial para a manutenção e colaboração eficazes. No projeto  **Aguilar-studies**, adotamos princípios de boas práticas de programação, como o  **DRY (Don't Repeat Yourself)**  e o  **SRP (Single Responsibility Principle)**. Isso resultou em um código mais legível, modular e fácil de entender. Comentários e documentação clara foram incorporados para facilitar a colaboração e a manutenção futura.\n\n## Tecnologias usadas\n- HTML5\n- CSS3\n- Javascript\n- Typescript\n- ReactJS",
   },
   {
     id: "14",
